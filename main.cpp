66fe886a9f34e3d4d89ca67f5adcd1b814cb3191
Madrajib Lab <madhabsharma94@gmail.com>
2024-12-07 16:02:13 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <Arduino.h>
+/* Step 1.
+* Replace with the serial print/write function
+* as per mcu
+*/
 #define PRINT  Serial.print
 #define READ  Serial.read
+/* Step 2 
+* Include the header
+*/
 #include "serial_prompt.h"
 
+/* Step 3
+* Add your cmd handlers
+* Note: command handler should has
+* int <handle_name>(int argc, char **argv)
+* definations.
+*/
 int help(int argc, char **argv)
 {
     Serial.println("Serial console commands:");
@@ -13,17 +26,26 @@ int help(int argc, char **argv)
     return 0;
 }
 
+/* Step 4
+* Add command name and its handler
+* to commands array
+*/
 cmd_t commands[] = {
   {"help", help}
 };
 
+/* keep this as it is */
 int commands_count = sizeof(commands)/ sizeof(cmd_t);
 
 void setup() {
   Serial.begin(115200);
+
   serial_greet();
 }
 
 void loop() {
+/* Step5:
+* Call it periodically
+*/
   serial_run();
 }
